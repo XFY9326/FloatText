8ce4195396e3f06894faf89b09585f23969ec9e8
XFY9326 <1069665464@qq.com>
2017-02-10 11:56:26 +0800

    Improve DateCount

--- app/src/main/java/tool/xfy9326/floattext/Method/FloatServiceMethod.java
+++ app/src/main/java/tool/xfy9326/floattext/Method/FloatServiceMethod.java
@@ -11,10 +11,10 @@ import java.net.*;
 import java.text.*;
 import java.util.*;
 import tool.xfy9326.floattext.*;
+import tool.xfy9326.floattext.Utils.*;
 
 import java.lang.Process;
 import java.text.ParseException;
-import tool.xfy9326.floattext.Utils.*;
 
 public class FloatServiceMethod
 {
@@ -23,9 +23,9 @@ public class FloatServiceMethod
 	public static String TEXT_ADVANCE_UPDATE_ACTION = "tool.xfy9326.floattext.Service.FloatAdvanceTextUpdateService.action.TEXT_ADVANCE_UPDATE_ACTION";
 	public static int DYNAMIC_LIST_VERSION = 6;
 	public static String ACTIVITY_CHANGE_ACTION = "tool.xfy9326.floattext.Service.FloatAdvanceTextUpdateService.action.ACTIVITY_CHANGE_ACTION";
-	
+
 	//动态变量更新列表
-	public static SharedPreferences setUpdateList (Context ctx)
+	public static SharedPreferences setUpdateList(Context ctx)
 	{
 		SharedPreferences list = ctx.getSharedPreferences("DynamicList", ctx.MODE_PRIVATE);
 		SharedPreferences.Editor list_editor = list.edit();
@@ -85,27 +85,27 @@ public class FloatServiceMethod
 		}
 		return list;
 	}
-	
+
 	//判断是否有字符
-	public static boolean hasWord (String all, String part)
+	public static boolean hasWord(String all, String part)
     {
         return all.contains(part);
     }
 
-	public static String[] StringtoStringArray (String str)
+	public static String[] StringtoStringArray(String str)
 	{
 		ArrayList<String> arr = FloatData.StringToStringArrayList(str);
         return arr.toArray(new String[arr.size()]);
 	}
 
-	public static int[] StringtoIntegerArray (String str)
+	public static int[] StringtoIntegerArray(String str)
 	{
 		ArrayList<Integer> arr = FloatData.StringToIntegerArrayList(str);
         return Itoi(arr.toArray(new Integer[arr.size()]));
 	}
 
 	//判断是否在启动器界面
-    public static boolean isHome (Context ctx, List<String> homes)
+    public static boolean isHome(Context ctx, List<String> homes)
     {
         ActivityManager mActivityManager = (ActivityManager)ctx.getSystemService(Context.ACTIVITY_SERVICE);
         List<ActivityManager.RunningTaskInfo> rti = mActivityManager.getRunningTasks(1);
@@ -121,7 +121,7 @@ public class FloatServiceMethod
     }
 
 	//布尔值对象转换
-	public static int[] Itoi (Integer[] B)
+	public static int[] Itoi(Integer[] B)
 	{
 		int[] b = new int[B.length];
 		for (int i = 0;i < B.length;i++)
@@ -132,7 +132,7 @@ public class FloatServiceMethod
 	}
 
 	//获取启动器包名
-    public static List<String> getHomes (Context ctx)
+    public static List<String> getHomes(Context ctx)
     {
         List<String> names = new ArrayList<String>();
         PackageManager packageManager = ctx.getPackageManager();
@@ -148,7 +148,7 @@ public class FloatServiceMethod
     }
 
 	//修复null
-	public static String fixnull (String str, String def)
+	public static String fixnull(String str, String def)
 	{
 		if (str == null)
 		{
@@ -158,18 +158,18 @@ public class FloatServiceMethod
 	}
 
 	//获取网速
-    public static long getTotalRxBytes (Context ctx)
+    public static long getTotalRxBytes(Context ctx)
     {
         return TrafficStats.getUidRxBytes(ctx.getApplicationInfo().uid) == TrafficStats.UNSUPPORTED ? 0 : (TrafficStats.getTotalRxBytes() / 1024);
     }
 
-    public static long getTotalTxBytes (Context ctx)
+    public static long getTotalTxBytes(Context ctx)
     {
         return TrafficStats.getUidRxBytes(ctx.getApplicationInfo().uid) == TrafficStats.UNSUPPORTED ? 0 : (TrafficStats.getTotalTxBytes() / 1024);
     }
 
 	//获取代理
-    public static boolean isVpnUsed ()
+    public static boolean isVpnUsed()
     {
         try
         {
@@ -197,7 +197,7 @@ public class FloatServiceMethod
 	}
 
 	//网速单位设置
-    public static String netspeedset (float speed)
+    public static String netspeedset(float speed)
     {
         String NS = "KB/s";
         if (speed >= 1024)
@@ -214,7 +214,7 @@ public class FloatServiceMethod
     }
 
 	//获取CPU运行率
-    public static int getProcessCpuRate ()
+    public static int getProcessCpuRate()
     {
         int rate = 0;
         try
@@ -246,7 +246,7 @@ public class FloatServiceMethod
     }
 
 	//获取内存状态
-    public static String getMeminfo (Context ctx)
+    public static String getMeminfo(Context ctx)
     {
         ActivityManager mam = (ActivityManager) ctx.getSystemService(Context.ACTIVITY_SERVICE);
         ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
@@ -258,144 +258,178 @@ public class FloatServiceMethod
     }
 
 	//日期计算
-    public static String datecount (Context ctx, String date)
+    public static String datecount(Context ctx, String date)
     {
         DateFormat format1 = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
         DateFormat format2 = new SimpleDateFormat("yyyy-MM-dd-HH-mm");
         DateFormat format3 = new SimpleDateFormat("yyyy-MM-dd-HH");
         DateFormat format4 = new SimpleDateFormat("yyyy-MM-dd");
-        String outputdate;
-        Date now = new Date();
-        Date set = new Date();
-        long diff = 0;
-        long days = 0;
+		long diff = 0;
+		diff = FormatCount(format1, date);
+		if (diff >= 0)
+		{
+			return DateCount_1(ctx, diff);
+		}
+		diff = FormatCount(format2, date);
+		if (diff >= 0)
+		{
+			return DateCount_2(ctx, diff);
+		}
+		diff = FormatCount(format3, date);
+		if (diff >= 0)
+		{
+			return DateCount_3(ctx, diff);
+		}
+		diff = FormatCount(format4, date);
+		if (diff >= 0)
+		{
+			return DateCount_4(ctx, diff);
+		}
+		else
+		{
+			return ctx.getString(R.string.dynamic_date_err);
+		}
+    }
+
+	private static long FormatCount(DateFormat format, String date)
+	{
+		try
+		{
+			Date now = new Date();
+			Date set = format.parse(date);
+			long diff = set.getTime() - now.getTime();
+			if(diff < 0)
+			{
+				diff = 0;
+			}
+			return diff;
+		}
+		catch (ParseException e)
+		{
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+	private static String DateCount_1(Context ctx, long diff)
+	{
+		long days = diff / (1000 * 60 * 60 * 24);
+		long hours = (diff - days * (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
+		long minutes = (diff - days * (1000 * 60 * 60 * 24) - hours * (1000 * 60 * 60)) / (1000 * 60);
+		long seconds = (diff - days * (1000 * 60 * 60 * 24) - hours * (1000 * 60 * 60) - minutes * (1000 * 60)) / 1000;
+		if (diff > 0)
+		{
+			if (days == 0)
+			{
+				if (hours == 0)
+				{
+					if (minutes == 0)
+					{
+						return seconds + ctx.getString(R.string.second);
+					}
+					else
+					{
+						return minutes + ctx.getString(R.string.minute) + seconds + ctx.getString(R.string.second);
+					}
+				}
+				else
+				{
+					return hours + ctx.getString(R.string.hour) + minutes + ctx.getString(R.string.minute) + seconds + ctx.getString(R.string.second);
+				}
+			}
+			else
+			{
+				return days + ctx.getString(R.string.day) + hours + ctx.getString(R.string.hour) + minutes + ctx.getString(R.string.minute) + seconds + ctx.getString(R.string.second);
+			}
+		}
+		else
+		{
+			return ctx.getString(R.string.dynamic_date_reach);
+		}
+	}
+
+	private static String DateCount_2(Context ctx, long diff)
+	{
+        long seconds = 0;
+		long days = diff / (1000 * 60 * 60 * 24);
+		long hours = (diff - days * (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
+		long minutes = (diff - days * (1000 * 60 * 60 * 24) - hours * (1000 * 60 * 60)) / (1000 * 60);
+		diff = diff - seconds * 1000;
+		if (diff > 0)
+		{
+			if (days == 0)
+			{
+				if (hours == 0)
+				{
+					return minutes + ctx.getString(R.string.minute);
+				}
+				else
+				{
+					return hours + ctx.getString(R.string.hour) + minutes + ctx.getString(R.string.minute);
+				}
+			}
+			else
+			{
+				return days + ctx.getString(R.string.day) + hours + ctx.getString(R.string.hour) + minutes + ctx.getString(R.string.minute);
+			}
+		}
+		else
+		{
+			return ctx.getString(R.string.dynamic_date_reach);
+		}
+	}
+
+	private static String DateCount_3(Context ctx, long diff)
+	{
+        long minutes = 0;
+        long seconds = 0;
+		long days = diff / (1000 * 60 * 60 * 24);
+		long hours = (diff - days * (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
+		diff = diff - minutes * 60 * 1000 - seconds * 1000;
+		if (diff > 0)
+		{
+			if (days == 0)
+			{
+				return hours + ctx.getString(R.string.hour);
+			}
+			else
+			{
+				return days + ctx.getString(R.string.day) + hours + ctx.getString(R.string.hour);
+			}
+		}
+		else
+		{
+			return ctx.getString(R.string.dynamic_date_reach);
+		}
+	}
+
+	private static String DateCount_4(Context ctx, long diff)
+	{
         long hours = 0;
         long minutes = 0;
         long seconds = 0;
-        try
-        {
-            set = format1.parse(date);
-            diff = set.getTime() - now.getTime();
-            days = diff / (1000 * 60 * 60 * 24);
-            hours = (diff - days * (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
-            minutes = (diff - days * (1000 * 60 * 60 * 24) - hours * (1000 * 60 * 60)) / (1000 * 60);
-            seconds = (diff - days * (1000 * 60 * 60 * 24) - hours * (1000 * 60 * 60) - minutes * (1000 * 60)) / 1000;
-            if (diff > 0)
-            {
-                if (days == 0)
-                {
-                    outputdate = hours + ctx.getString(R.string.hour) + minutes + ctx.getString(R.string.minute) + seconds + ctx.getString(R.string.second);
-                    if (hours == 0)
-                    {
-                        outputdate = minutes + ctx.getString(R.string.minute) + seconds + ctx.getString(R.string.second);
-                        if (minutes == 0)
-                        {
-                            outputdate = seconds + ctx.getString(R.string.second);
-                        }
-                    }
-                }
-                else
-                {
-                    outputdate = days + ctx.getString(R.string.day) + hours + ctx.getString(R.string.hour) + minutes + ctx.getString(R.string.minute) + seconds + ctx.getString(R.string.second);
-                }
-            }
-            else
-            {
-                outputdate = ctx.getString(R.string.dynamic_date_reach);
-            }
-        }
-        catch (Exception e1)
-        {
-            try
-            {
-                set = format2.parse(date);
-                diff = set.getTime() - now.getTime();
-                days = diff / (1000 * 60 * 60 * 24);
-                hours = (diff - days * (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
-                minutes = (diff - days * (1000 * 60 * 60 * 24) - hours * (1000 * 60 * 60)) / (1000 * 60);
-                diff = diff - seconds * 1000;
-                if (diff > 0)
-                {
-                    if (days == 0)
-                    {
-                        outputdate = hours + ctx.getString(R.string.hour) + minutes + ctx.getString(R.string.minute);
-                        if (hours == 0)
-                        {
-                            outputdate = minutes + ctx.getString(R.string.minute);
-                        }
-                    }
-                    else
-                    {
-                        outputdate = days + ctx.getString(R.string.day) + hours + ctx.getString(R.string.hour) + minutes + ctx.getString(R.string.minute);
-                    }
-                }
-                else
-                {
-                    outputdate = ctx.getString(R.string.dynamic_date_reach);
-                }
-            }
-            catch (ParseException e2)
-            {
-                try
-                {
-                    set = format3.parse(date);
-                    diff = set.getTime() - now.getTime();
-                    days = diff / (1000 * 60 * 60 * 24);
-                    hours = (diff - days * (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
-                    diff = diff - minutes * 60 * 1000 - seconds * 1000;
-                    if (diff > 0)
-                    {
-                        if (days == 0)
-                        {
-                            outputdate = hours + ctx.getString(R.string.hour);
-                        }
-                        else
-                        {
-                            outputdate = days + ctx.getString(R.string.day) + hours + ctx.getString(R.string.hour);
-                        }
-                    }
-                    else
-                    {
-                        outputdate = ctx.getString(R.string.dynamic_date_reach);
-                    }
-                }
-                catch (ParseException e3)
-                {
-                    try
-                    {
-                        set = format4.parse(date);
-                        diff = set.getTime() - now.getTime();
-                        days = diff / (1000 * 60 * 60 * 24);
-                        diff = diff - hours * 60 * 60 * 1000 - minutes * 60 * 1000 - seconds * 1000;
-                        if (diff > 0)
-                        {
-                            outputdate = days + ctx.getString(R.string.day);
-                        }
-                        else
-                        {
-                            outputdate = ctx.getString(R.string.dynamic_date_reach);
-                        }
-                    }
-                    catch (ParseException e4)
-                    {
-                        outputdate = ctx.getString(R.string.dynamic_date_err);
-                        e4.printStackTrace();
-                    }
-                }
-            }
-        }
-        return outputdate;
-    }
+		long days = diff / (1000 * 60 * 60 * 24);
+		diff = diff - hours * 60 * 60 * 1000 - minutes * 60 * 1000 - seconds * 1000;
+		if (diff > 0)
+		{
+			return days + ctx.getString(R.string.day);
+		}
+		else
+		{
+			return ctx.getString(R.string.dynamic_date_reach);
+		}
+	}
+
 
 	//判断屏幕开启
-    public static boolean isScreenOn (Context ctx)
+    public static boolean isScreenOn(Context ctx)
     {
         PowerManager pm = (PowerManager)ctx.getSystemService(Context.POWER_SERVICE);
         return pm.isScreenOn();
     }
 
 	//获取IP
-    public static String getIP (Context ctx)
+    public static String getIP(Context ctx)
     {
         WifiManager wifiService = (WifiManager) ctx.getSystemService(ctx.WIFI_SERVICE);
         WifiInfo wifiinfo = wifiService.getConnectionInfo();
@@ -403,7 +437,7 @@ public class FloatServiceMethod
     }
 
 	//int转IP
-    public static String intToIp (int i)
+    public static String intToIp(int i)
     {
         return (i & 0xFF) + "." + ((i >> 8) & 0xFF) + "." + ((i >> 16) & 0xFF) + "." + (i >> 24 & 0xFF);
     }
